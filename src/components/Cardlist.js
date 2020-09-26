b4466c40e35271a415a09a59707c49579c4b3419
juned123 <juned@frrole.com>
2020-09-26 17:22:52 +0530

    filtering the data coming as props to dropdown to show in the cardlist is done

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -5,7 +5,8 @@ import { Button,Container,Navbar,Nav} from 'react-bootstrap';
 import './common.css'
 
 function MovieCard(props) {
-
+ // console.log(props)
+  const {movies}=props
   
 
   return (
@@ -13,11 +14,11 @@ function MovieCard(props) {
 
 <div className="card" style={{width: "18rem"}}>
   {/* <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" role="img"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg> */}
-  <img className="bd-placeholder-img card-img-top" width="100%" height="180"  src={"https://m.media-amazon.com/images/M/MV5BNDYxNWUzZmYtOGQxMC00MTdkLTkxOTctYzkyOGIwNWQxZjhmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"}/>
+  <img className="bd-placeholder-img card-img-top" width="100%" height="180"  src={movies.Poster}/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Quick sample text to create the card title and make up the body of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{movies.Title}</h5>
+  <p className="card-text">{movies.Year}</p>
+    <a href="#" className="btn btn-primary">Add Favourite</a>
   </div>
 {/* </div> */}
 
