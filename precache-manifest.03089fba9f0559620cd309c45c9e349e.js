26bd13c74c50daacea217b66587aa1d9fcc3a981
juned123 <juned@frrole.com>
2020-09-27 00:46:17 +0530

    Updates

---
+++ precache-manifest.03089fba9f0559620cd309c45c9e349e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab9ba242b58e2c9d99eb70698084743",
+    "url": "/movie-assingnment/index.html"
+  },
+  {
+    "revision": "b887ad71b4f762133b8d",
+    "url": "/movie-assingnment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "67b395b54984ed883163",
+    "url": "/movie-assingnment/static/css/main.8145fff5.chunk.css"
+  },
+  {
+    "revision": "b887ad71b4f762133b8d",
+    "url": "/movie-assingnment/static/js/2.e9ed14bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-assingnment/static/js/2.e9ed14bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b395b54984ed883163",
+    "url": "/movie-assingnment/static/js/main.e4ad7833.chunk.js"
+  },
+  {
+    "revision": "72c8c93f6ba5480a6ee9",
+    "url": "/movie-assingnment/static/js/runtime-main.f277fe63.js"
+  }
+]);
